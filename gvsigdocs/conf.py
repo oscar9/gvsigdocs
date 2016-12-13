a1c763f4ea29cf2249a1f76892c95db9a488be41
oscar9 <masquesig@gmail.com>
2016-12-13 10:56:49 +0100

    up

--- gvsigdocs/conf.py
+++ gvsigdocs/conf.py
@@ -74,7 +74,7 @@ release = u'0'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-locale_dirs = ['locale/']  
+locale_dirs = ['locale/','../gvsigdocs-english/']  
 gettext_compact = False
 language = ['es','en']
 
