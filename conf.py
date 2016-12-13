826f5876f3d251ba844bd3f78dca8f5627a898e5
oscar9 <masquesig@gmail.com>
2016-12-13 12:01:35 +0100

    Updating config files

--- conf.py
+++ conf.py
@@ -168,13 +168,13 @@ html_short_title = "gvSIG Documentation"
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #
-html_logo = "config/gvsig_asoc.jpg"
+html_logo = "_config/gvsig_asoc.jpg"
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
 #
-html_favicon = "config/gvSIG.ico"
+html_favicon = "_config/gvSIG.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
